ce1f9598c948121d90647b4304dd649ec1923e73
johnpwillman <johnpwillman@gmail.com>
2014-03-03 16:55:05 -0800

    Add HeapIterator class for later implementation.

---
+++ src/HeapIterator.java
@@ -0,0 +1,28 @@
+import java.util.Iterator;
+
+
+public class HeapIterator implements Iterator<String> {
+	
+	public HeapIterator(HeapNode rootNode) {
+		
+	}
+
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remove() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
