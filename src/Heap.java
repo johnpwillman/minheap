478292296668f65b7a9839a9ec6b545943140599
johnpwillman <johnpwillman@gmail.com>
2014-03-04 10:48:48 -0800

    Update comments in Heap class to reference Heap instead of MinHeap. Add Javadoc comments to public methods that need it.

--- src/Heap.java
+++ src/Heap.java
@@ -2,7 +2,7 @@ import java.util.AbstractCollection;
 import java.util.Iterator;
 
 /**
- * The MinHeap class implements a Min-Heap data structure of Strings where root has the
+ * The Heap class implements a Heap data structure of Strings where root has the
  * smallest value in the heap and each node is smaller than all the values in the heaps
  * of its child nodes. Each node has a maximum of two children.
  * This implementation allows adding of nodes to the heap and printing of the heap's
@@ -18,14 +18,14 @@ public class Heap extends AbstractCollection<String>{
 	private HeapNode rootNode = new NullHeapNode();
 	
 	/**
-	 * Instantiates an empty MinHeap object
+	 * Instantiates an empty Heap object
 	 */
 	public Heap() {
 		
 	}
 	
 	/**
-	 * Instantiates a MinHeap object with heapRootValue as the value of the root node.
+	 * Instantiates a Heap object with heapRootValue as the value of the root node.
 	 * 
 	 * @param heapRootValue	The desired value of the root node.
 	 */
@@ -33,8 +33,9 @@ public class Heap extends AbstractCollection<String>{
 		add(heapRootValue);
 	}
 	
-	/* (non-Javadoc)
-	 * @see java.util.AbstractCollection#iterator()
+	/**
+	 * Returns an Iterator for this Heap Collection.
+	 * Iterator is of String type.
 	 */
 	@Override
 	public Iterator<String> iterator() {
@@ -43,15 +44,15 @@ public class Heap extends AbstractCollection<String>{
 	}
 
 	/**
-	 * 
+	 * Return the current number of items contained in this Heap Collection.
 	 */
 	@Override
 	public int size() {
 		return getRootNode().getHeapSize();
 	}
 
-	/* (non-Javadoc)
-	 * @see java.util.AbstractCollection#toArray()
+	/**
+	 * Returns a String array containing this Heap Collections items.
 	 */
 	@Override
 	public String[] toArray() {
@@ -68,17 +69,18 @@ public class Heap extends AbstractCollection<String>{
 		return heapArray;
 	}
 
-	/* (non-Javadoc)
-	 * @see java.util.AbstractCollection#add(java.lang.Object)
+	/**
+	 * Adds a new String value to the Heap.
 	 */
+	@Override
 	public boolean add(String newHeapValue) {
 		// TODO Return false if add was unsuccessful
 		rootNode = getRootNode().add(newHeapValue);
 		return true;
 	}
 
-	/* (non-Javadoc)
-	 * @see java.util.AbstractCollection#toString()
+	/**
+	 * Returns a String representation of the Heap Collection's values.
 	 */
 	@Override
 	public String toString() {
